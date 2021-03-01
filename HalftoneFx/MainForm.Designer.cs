9c7f4bcecaf49a4fe4714c0e0489f53f760d2cd5
HorobryhKS <kirson@mail.com>
2021-03-01 15:23:00 +0300

    Added application icon

--- HalftoneFx/MainForm.Designer.cs
+++ HalftoneFx/MainForm.Designer.cs
@@ -37,6 +37,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(971, 561);
             this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HalftoneFx";
--- HalftoneFx/Properties/Resources.Designer.cs
+++ HalftoneFx/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace HalftoneFx.Properties {
         }
         
         /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon Icon {
+            get {
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Logo {
